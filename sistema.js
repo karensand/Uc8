d185a25a5c04197ff43e863e7d49c2352af64324
karensand <ksilva@mtsolucoes.com.br>
2022-04-22 14:29:50 -0300

    Encontro 09/12 Sistema de Alunos

---
+++ sistema.js
@@ -0,0 +1,37 @@
+let numeroDeAlunos = ["Karen","Bianca","Felipe","Mateus"]
+
+for (let nome of numeroDeAlunos) {
+    console.log(`Esta pessoa se chama ${nome}`)
+}
+
+
+
+// let numeroDeAlunos = 10;
+
+// let contador = 0;
+
+// while(contador <= numeroDeAlunos) {
+//   //console.log(contador)
+
+//   if(contador == 0){
+//       console.log("O número atual é zero")
+//   }else if(contador % 2 == 0){
+//       console.log("O número " + contador + " é Par")
+//   }else{
+//       console.log(`O numero ${contador} é Impar`)
+//   } 
+//   contador++;
+// }
+
+
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+//     //console.log(contador)
+  
+//     if(contador == 0){
+//         console.log("O número atual é zero")
+//     }else if(contador % 2 == 0){
+//         console.log("O número " + contador + " é Par")
+//     }else{
+//         console.log(`O numero ${contador} é Impar`)
+//     } 
+//   }
