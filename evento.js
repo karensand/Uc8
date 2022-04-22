0cd87540679b56417700181652d19ec664ef5b7c
karensand <ksilva@mtsolucoes.com.br>
2022-04-22 12:06:41 -0300

    Encontro Remoto 1

---
+++ evento.js
@@ -0,0 +1,25 @@
+var CadastroDoEvento = [25/04/2022]
+
+if (CadastroDoEvento.length < 24) {
+    // É possível cadastrar mais participantes
+    console.log("É possível cadastrar mais participantes")
+} else {
+    console.log("Data limite para cadastro ultrapassada")
+}
+ 
+
+let idade = 18;
+if (idade >= 18) {
+    //Cadastro autorizado
+    console.log("Cadastro autorizado")
+} else {
+    console.log("Cadastro não permitido")
+}
+
+let participantes = 100;
+if (participantes <= 100) {
+    //Permitir cadastro
+    console.log("Permitir e registrar participante")
+} else {
+    console.log("Quantidade de vagas encerrada")
+}
